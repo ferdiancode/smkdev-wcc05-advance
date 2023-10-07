af9b3214a374c5ac6f2fa5015c2a89066a0cb61c
Ferdi <143518405+ferdiancode@users.noreply.github.com>
2023-10-07 23:55:57 +0700

    upload files

---
+++ weekly-coding-challenge-05/advance.js
@@ -0,0 +1,13 @@
+//SMKDEV
+//Weekly Coding Challenge #05
+//Advance Level
+
+//Mohammad Ferdian Al Fahrezi
+
+const input_number = 6;
+
+for (let i = 1; i <= input_number; i++) {
+  const cube = i * i * i;
+  console.log(`Current Number is: ${i} and the cube is ${cube}`);
+}
+
